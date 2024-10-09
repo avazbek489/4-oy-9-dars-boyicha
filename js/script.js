6b14c6473683c65d44c44c88b97223b997008f24
avazbek489 <obidjonovavazbek787@gmail.com>
2024-10-09 17:06:52 +0500

    All questions have been developed

---
+++ js/script.js
@@ -0,0 +1,180 @@
+// // // Oddiy masalalar
+// // 1-MASALA JAVOBI
+let arr = ["Toyota", "BMW", 450, "Mercedes", 520, "Pagani"];
+console.log(arr[0]);
+console.log(arr[5]);
+
+// // 2-MASALA JAVOBI
+//  let arr = ["Toyota", "BMW", 450, "Mercedes", 520, "Pagani"];
+// arr[4] = "Salom"
+// console.log(arr);
+
+// // 3-MASALA JAVOBI
+// let arr = [];
+// arr.push("Olma", "Behi","Uzum")
+// console.log(arr);
+
+// // 4-MASALA JAVOBI
+//  let arr = ["Toyota", "BMW", 450, "Mercedes", 520, "Pagani"];
+// console.log(arr.length);
+
+// // 5-MASALA JAVOBI
+// let arr = ["Toyota", "BMW", 450, "Mercedes", 520, "Pagani"];
+// arr.push("Audi")
+
+// console.log(arr[6]);
+
+// // 6-MASALA JAVOBI
+// let arr = ["Toyota", "BMW", 450, "Mercedes", 520, "Pagani"];
+// arr.pop();
+// console.log(arr);
+
+// // 7-MASALA JAVOBI
+// let arr = ["Toyota", "BMW", 450, "Mercedes", 520, "Pagani"];
+// arr.unshift("Lamborghini");
+// console.log(arr);
+
+// // 8-MASALA JAVOBI
+// let arr = ["Toyota", "BMW", 450, "Mercedes", 520, "Pagani"];
+// arr.shift();
+// console.log(arr);
+
+// // 9-MASALA JAVOBI
+// let arr = [16, 25, 39, 43, 54];
+// for (let i = 0; i < arr.length; i++) {
+//   console.log(arr[i]);
+// }
+
+// // 10-MASALA JAVOBI
+// let arr = [];
+// if (arr.length > 0) {
+//   alert("mavjud");
+// } else {
+//   alert("mavjud emas");
+// }
+
+// // // For loop ga oid masalalar
+// // 1-MASALA JAVOBI
+// let arr = [
+//   "Toyota",
+//   42,
+//   "BMW",
+//   100,
+//   "Mercedes",
+//   76,
+//   "Pagani",
+//   5,
+//   "Lamborghini",
+//   89,
+// ];
+// for (let i = 0; i < arr.length; i++) {
+//   console.log(arr[i]);
+// }
+
+// // 2-MASALA JAVOBI
+// let arr = ["Olma", "Banan", "Apelsin", "Uzum", "Anor"];
+// for (let i = 0; i < arr.length; i++) {
+//   console.log(arr[i]);
+// }
+
+// // 3-MASALA JAVOBI
+// let arr = [25, 42, 37, 58, 91, 84, 102, 73, 66, 49];
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] % 2 == 0) {
+//     console.log(arr[i]);
+//   }
+// }
+
+// // 4-MASALA JAVOBI
+// let arr = [25, 42, 37, 58, 91, 84, 102, 73, 66, 49];
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] % 2 == 1) {
+//     console.log(arr[i]);
+//   }
+// }
+
+// // 5-MASALA JAVOBI
+// let arr = [25, 42, -37, 58, 91, 84, -102, 73, -66, 49];
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] > 0) {
+//     console.log(arr[i]);
+//   }
+// }
+// // 6-MASALA JAVOBI
+// let arr = [5, 12, 37, 45, 23, 78, 56, 89, 11, 34];
+// for (let i = arr.length - 1; i >= 0; i--) {
+//   console.log(arr[i]);
+// }
+
+// // 7-MASALA JAVOBI
+// let arr = [5, 12, 37, 45, 23, 78, 56, 89, 11, 34];
+// sum = 0;
+
+// for (let i = 0; i < arr.length; i++) {
+//   sum += arr[i];
+// }
+// console.log(sum);
+
+// // 8-MASALA JAVOBI
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// for (let i = 0; i < arr.length; i++) {
+//   arr[i] *= 2;
+// }
+// console.log(arr);
+
+// // 9-MASALA JAVOBI
+// let arr = [1, 58, 390, 999, 223, 8];
+// let max = arr[0];
+// for (let i = 1; i < arr.length; i++) {
+//   if (arr[i] > max) {
+//     max = arr[i];
+//   }
+// }
+// console.log(max);
+
+// // 10-MASALA JAVOBI
+// let arr = [10, 58, 30, 49, 83, 8];
+// let min = arr[0];
+// for (let i = 1; i < arr.length; i++) {
+//   if (arr[i] < min) {
+//     min = arr[i];
+//   }
+// }
+// console.log(min);
+
+// // 11-MASALA JAVOBI
+// let arr = [-5, 3, -2, 8, -1, 4];
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] < 0) {
+//     console.log(arr[i]);
+//   }
+// }
+
+// // 12-MASALA JAVOBI
+// let arr = [-5, 3, -2, 8, -1, 4];
+// let sum = 0;
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] > 0) {
+//     sum += arr[i];
+//   }
+// }
+// alert(sum);
+
+// // 13-MASALA JAVOBI
+// let arr = [13, 223, 123, 3124, 54, 234];
+
+// for (let i = 0; i < arr.length; i++) {
+//   console.log(`${arr[i]}  ${i} `);
+// }
+
+// // 14-MASALA JAVOBI
+// let arr = [5, -3, 10, 0, 25, -8, 15];
+// for (let i = arr.length - 1; i >= 0; i--) {
+//   console.log(`${arr[i]} ${i}`);
+// }
+
+// // 15-MASALA JAVOBI
+// let arr = [5, -3, 10, 0, 25, -8, 15];
+// for (let i = 1; i < arr.length; i++) {
+//   console.log(arr[i]);
+// }
